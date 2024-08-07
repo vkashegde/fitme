8a9cf13232bd6c8edc27eb4d3e0bf0efeb9bd44f
vkashegde <vikas@kofluence.com>
2024-08-07 21:32:51 +0530

    web and mobile nav bar completion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <div className="app bg-gray-20 ">
-        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        <Navbar
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
       </div>
     </>
   );
---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelelctedPage: (value: SelectedPage) => void;
+};
+
+const ActionButton = ({ children, setSelelctedPage }: Props) => {
+  return (
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+      onClick={() => setSelelctedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
